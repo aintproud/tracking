e4afd3c1614d040090efdbda88d116359875f8bf
John Doe <johndoe@example.com>
2024-02-17 02:29:39 +1000

    some changes

--- src/modules/ws/messageFabric.mjs
+++ src/modules/ws/messageFabric.mjs
@@ -1,14 +1,11 @@
 import SomeHandler from './types/some.mjs'
 
 export default class MessageFabric {
-  constructor(json, connection) {
-    this.json = json
-    this.connection = connection
+  constructor({ type, data }, connection) {
     const router = {
       some: SomeHandler,
     }
-    const { type, data } = json
-    const targetClass = router[type] ? router[type] : SomeHandler
+    const targetClass = router[type] || SomeHandler
     return new targetClass(data, connection)
   }
 }
