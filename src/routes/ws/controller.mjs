b117bb37f0f751830db0d89c6a3c5507728b1af7
John Doe <johndoe@example.com>
2024-02-19 05:06:55 +1000

    message handling incapsulation

--- src/routes/ws/controller.mjs
+++ src/routes/ws/controller.mjs
@@ -1,11 +1,8 @@
-import MessageFabric from './messageFabric.mjs'
 import { asyncStorageBinding } from 'src/modules/middlewares.mjs'
 import { jwtGuard } from 'src/modules/guards.mjs'
 import asyncLocalStorage from 'src/modules/async.mjs'
 import schema from './schema.mjs'
-import { createResponse, primaryValidation } from './wsUtils.mjs'
-import classes from './classesLoader.mjs'
-
+import RunTime from './runtime.mjs'
 export default async function (fastify) {
   fastify.get(
     '/',
@@ -17,28 +14,8 @@ export default async function (fastify) {
     (connection) => {
       const context = asyncLocalStorage.getStore()
       connection.socket.on('message', (message) => {
-        const json = primaryValidation(message)
-        if (!json)
-          return connection.socket.send(
-            createResponse({ description: 'invalid message' }, true)
-          )
-        const targetObject = MessageFabric(json, context, connection)
-        if (!targetObject)
-          return connection.socket.send(
-            createResponse(
-              {
-                description: `invalid message type: ${json.type}`,
-                proposes: classes.map((c) => c.type),
-              },
-              true
-            )
-          )
-        const valid = targetObject.validate()
-        if (!valid)
-          return connection.socket.send(
-            createResponse(targetObject.createValidationErrorData(), true)
-          )
-        targetObject.handle()
+        const runTime = new RunTime(message, context, connection)
+        runTime.run()
       })
     }
   )
---
+++ src/routes/ws/runtime.mjs
@@ -0,0 +1,68 @@
+import Ajv from "ajv"
+import MessageFabric from './messageFabric.mjs'
+import classes from './classesLoader.mjs'
+import { createResponse } from "./wsUtils.mjs"
+const ajv = new Ajv()
+
+export default class RunTime {
+  constructor(message,context, connecion) {
+    this.context = context
+    this.connecion = connecion
+    this.message = message
+    this.primarySchema = {
+      type: 'object',
+      properties: {
+        type: { type: 'string' },
+        data: { type: 'object' },
+      },
+      required: ['type', 'data'],
+      additionalProperties: false,
+    }
+  }
+  primaryValidation(message) {
+    try {
+      const json = JSON.parse(message)
+      const valid = ajv.validate(this.primarySchema, json)
+      return !valid || json
+    } catch (e) {
+      return false
+    }
+  }
+  createPrimaryValidationErrorData() {
+    return {
+      description: 'invalid message format',
+      schema: this.primarySchema,
+      recieved: this.message,
+    }
+  }
+  createWrongTypeErrorData() {
+    return {
+      description: `invalid message type: ${this.json.type}`,
+      proposes: classes.map((c) => c.type),
+    }
+  }
+  setHandler(){
+    this.handler = MessageFabric(this.json, this.context, this.connecion)
+  }
+  run(){
+    this.json = this.primaryValidation(this.message)
+    if (!this.json)
+      return this.connecion.socket.send(
+        createResponse(this.createPrimaryValidationErrorData(), true)
+      )
+    this.setHandler()
+    if (!this.handler)
+      return this.connecion.socket.send(
+        createResponse(
+          this.createWrongTypeErrorData(),
+          true
+        )
+      )
+    const valid = this.handler.validate()
+    if (!valid)
+      return this.connecion.socket.send(
+        createResponse(this.handler.createValidationErrorData(), true)
+      )
+    this.handler.handle()
+  }
+}
