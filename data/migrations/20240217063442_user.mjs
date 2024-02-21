986e68e705fbd5258acf49c478900330dc6f7a98
John Doe <johndoe@example.com>
2024-02-21 13:51:37 +1000

    make migrations and seeds

---
+++ data/migrations/20240217063442_user.mjs
@@ -0,0 +1,11 @@
+export const up = async (knex) => {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary()
+    table.string('name').notNullable()
+    table.string('email').notNullable()
+    table.string('password_hash').notNullable()
+  })
+}
+export const down = async (knex) => {
+  return knex.schema.dropTable('users')
+}
---
+++ data/migrations/20240220140000_validation_token.mjs
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async (knex) => {
+  return knex.schema.createTable('validation_token', (table) => {
+    table.integer('user_id').unsigned().references('id').inTable('users')
+    table.string('token')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = async (knex) => {
+  return knex.schema.dropTable('validation_token')
+}
