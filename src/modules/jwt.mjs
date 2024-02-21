c1deb3c809308d5de5d317b2ebe6cb7b38a6bba2
John Doe <johndoe@example.com>
2024-02-22 02:54:35 +1000

    types

--- src/modules/jwt.mjs
+++ src/modules/jwt.mjs
@@ -1,12 +1,12 @@
-import jwt from 'jsonwebtoken'
+import { sign, verify } from 'jsonwebtoken'
 import config from 'src/config.mjs'
 
 const { secret, daysToExpire } = config.jwt
 export function createToken(payload) {
-  return jwt.sign(payload, secret, { expiresIn: `${daysToExpire}d` })
+  return sign(payload, secret, { expiresIn: `${daysToExpire}d` })
 }
 
 export function verifyToken(payload) {
-  const { exp, iat, ...body } = jwt.verify(payload, secret)
+  const { exp, iat, ...body } = verify(payload, secret)
   return body
 }
