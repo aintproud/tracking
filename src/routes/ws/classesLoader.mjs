56bed67d24b91fd51ff424931d3197726246ea26
John Doe <johndoe@example.com>
2024-02-25 13:16:41 +1000

    less files

--- src/routes/ws/classesLoader.mjs
+++
@@ -1,8 +0,0 @@
-import { readdirSync } from 'node:fs'
-const folder = 'classes'
-const files = readdirSync(`${import.meta.dirname}/${folder}`)
-const filteredFiles = files.filter((file) => file.endsWith('.mjs'))
-
-export default (
-  await Promise.all(filteredFiles.map((file) => import(`./${folder}/${file}`)))
-).map((r) => r.default)
--- src/routes/ws/runtime.mjs
+++ src/routes/ws/runtime.mjs
@@ -1,4 +1,11 @@
 import Runtime from "src/modules/ws/runtime.mjs";
-import classes from "./classesLoader.mjs";
+import { readdirSync } from 'node:fs'
+const folder = 'classes'
+const files = readdirSync(`${import.meta.dirname}/${folder}`)
+const filteredFiles = files.filter((file) => file.endsWith('.mjs'))
+
+const classes = (
+  await Promise.all(filteredFiles.map((file) => import(`./${folder}/${file}`)))
+).map((r) => r.default)
 
 export default new Runtime(classes)
